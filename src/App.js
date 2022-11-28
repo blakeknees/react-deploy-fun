a10394a2028e8ea5cb2b1e4db3a20f6c80cdfdb6
blakeknees <jenniferblakney@gmail.com>
2022-11-28 12:19:44 -0500

    aaron deploy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hiiii welcome to production branch baby!</h1>
+      <h2>Aaron tried this with a react app before and it was sooo complicated.</h2>
+      <h3>Now this is so easy</h3>
     </div>
   );
 }
